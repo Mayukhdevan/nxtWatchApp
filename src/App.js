bc9ef7d5e297082a0cf21594ca16d741d3fb776f
Mayukhdevan <mayukhdevansahu@yahoo.com>
2023-04-27 13:48:32 +0000

    all routes inside app.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css'
-import {Route, Switch, Redirect, useLocation} from 'react-router-dom'
+import {Route, Switch, Redirect} from 'react-router-dom'
 import Login from './components/Login'
-import Sidebar from './components/Sidebar'
 import Home from './components/Home'
 import Trending from './components/Trending'
 import Gaming from './components/Gaming'
@@ -10,26 +9,17 @@ import VideoItemDetails from './components/VideoItemDetails'
 import NotFound from './components/NotFound'
 
 // Replace your code here
-const App = () => {
-  const location = useLocation()
-  const {pathname} = location
-  const showSidebar = pathname !== '/login' && pathname !== '/bad-path'
-
-  return (
-    <>
-      {showSidebar && <Sidebar />}
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/trending" component={Trending} />
-        <Route exact path="/gaming" component={Gaming} />
-        <Route exact path="/saved-videos" component={SavedVideos} />
-        <Route exact path="/videos/:id" component={VideoItemDetails} />
-        <Route exact path="/bad-path" component={NotFound} />
-        <Redirect to="/bad-path" />
-      </Switch>
-    </>
-  )
-}
+const App = () => (
+  <Switch>
+    <Route exact path="/" component={Home} />
+    <Route exact path="/login" component={Login} />
+    <Route exact path="/trending" component={Trending} />
+    <Route exact path="/gaming" component={Gaming} />
+    <Route exact path="/saved-videos" component={SavedVideos} />
+    <Route exact path="/videos/:id" component={VideoItemDetails} />
+    <Route exact path="/bad-path" component={NotFound} />
+    <Redirect to="/bad-path" />
+  </Switch>
+)
 
 export default App
